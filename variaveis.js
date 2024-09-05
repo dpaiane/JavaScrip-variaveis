7b5cd291f0885bdb2c9c5ccf315de8cb02ab8a48
dpaiane <diegocardosoengenharia@gmail.com>
2024-09-05 08:39:27 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+var Meu_nome = "Diego";
+var Nome = "Paiani";
+const numeroPi = 3.14;
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+
+console.log(numeroPi);
+console.log(typeof(Meu_nome));
+console.log(tr);
